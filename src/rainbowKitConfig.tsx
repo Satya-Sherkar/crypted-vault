07a8cd2d062f00c2c72d1e56c5c96be7f86fecb0
Satyam Sherkar <satyamsherkar007@gmail.com>
2026-01-03 16:50:47 +0530

    feat: update RainbowKit app name  to "Crypted Vault"

--- src/rainbowKitConfig.tsx
+++ src/rainbowKitConfig.tsx
@@ -4,7 +4,7 @@ import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import { sepolia, anvil, mainnet } from "wagmi/chains";
 
 export default getDefaultConfig({
-    appName: "Carbon Marketplace",
+    appName: "Crypted Vault",
     projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
     chains: [sepolia, anvil, mainnet],
     ssr: false,
