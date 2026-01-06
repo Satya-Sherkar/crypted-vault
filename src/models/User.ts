ebf5704fc376121858d50da44a6c9e3633536597
Satyam Sherkar <satyamsherkar007@gmail.com>
2026-01-06 17:02:31 +0530

    feat: add User model with schema definition for MongoDB

---
+++ src/models/User.ts
@@ -0,0 +1,27 @@
+import mongoose, { Schema, model, models } from 'mongoose';
+
+const UserSchema = new Schema(
+    {
+        clerkId: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        firstName: {
+            type: String,
+        },
+        lastName: {
+            type: String,
+        },
+    },
+    { timestamps: true }
+);
+
+const User = models.User || model('User', UserSchema);
+
+export default User;
